36498afa6e4ec8ec62f45893b5e3b24dddb64c37
Adam Pah <adamrpah@gmail.com>
2011-06-29 20:28:47 -0400

    changed production environment for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ NpCompleteHeart::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'ice-cold-955.heroku.com' }
 end
