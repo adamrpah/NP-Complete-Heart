edd43d35fff183c0505fa41f818a70de48da2141
Adam Pah <adamrpah@gmail.com>
2011-06-29 21:56:10 -0400

    added new file for devise to try and enable users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 NpCompleteHeart::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
 
   resources :posts do
     resources :comments
