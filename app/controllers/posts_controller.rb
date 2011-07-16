c87760f90c64141778c8a9ba6c0f395240f9750d
Adam Pah <adamrpah@gmail.com>
2011-07-16 14:54:49 -0400

    fixed devise issue by changing the command code in the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_filter :authenticate_user!, :except => [:index, :show]
+  skip_before_filter :authenticate_user!, :except => [:front, :index, :show]
   
   # GET /posts
   # GET /posts.xml
